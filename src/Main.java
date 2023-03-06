aac5de46bf9f506d2e16ae8b9d3703f16c9fc82d
Constantine-Medved <medved.k@outlook.com>
2023-03-06 21:35:33 +0300

    Task 1 is done

--- src/Main.java
+++ src/Main.java
@@ -10,30 +10,36 @@ public class Main {
     }
 
     public static void task1() {
-
+        System.out.println("Задача 1.");
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + " лет, он совершеннолетний.");
+        } else {
+            System.out.println("Если возраст человека равен " + age + " лет,он не достиг совершеннолетия, нужно немного подождать.");
+        }
     }
 
     public static void task2() {
-
+        System.out.println("Задача 2.");
     }
 
     public static void task3() {
-
+        System.out.println("Задача 3.");
     }
 
     public static void task4() {
-
+        System.out.println("Задача 4.");
     }
 
     public static void task5() {
-
+        System.out.println("Задача 5.");
     }
 
     public static void task6() {
-
+        System.out.println("Задача 6.");
     }
 
     public static void task7() {
-
+        System.out.println("Задача 7.");
     }
 }
