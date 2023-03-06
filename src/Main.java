7572c864d64381198d1eb2ef08bac511664d1fdd
Constantine-Medved <medved.k@outlook.com>
2023-03-06 23:54:03 +0300

    Task 5 is done

--- src/Main.java
+++ src/Main.java
@@ -36,13 +36,43 @@ public class Main {
             System.out.println("Если скорость " + speed + " км/ч, можно ездить спокойно.");
         }
     }
-
     public static void task4() {
         System.out.println("Задача 4.");
+        int age = 20;
+        if (age < 2) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно быть дома с мамой.");
+        }
+        if (age >= 2 && age <= 6) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в садик.");
+        }
+        if (age >= 7 && age <= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу.");
+        }
+        if (age > 18 && age < 24) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в университет.");
+        }
+        if (age >= 24) {
+            System.out.println("Если возраст человека равен " + age + ", то ему пора ходить на работу.");
+        }
     }
 
     public static void task5() {
         System.out.println("Задача 5.");
+        int age = 12;
+        boolean withAdult = false;
+        if (age < 5) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему нельзя кататься на аттракционе.");
+        }
+        if (age >=5 && age <14 && withAdult) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе в сопровождении взрослого.");
+        } else {
+            if (age >=5 && age <14 && !withAdult) {
+                System.out.println("Если возраст ребенка равен " + age + ", то ему нельзя кататься на аттракционе, нет сопровождающего взрослого.");
+        }
+        if (age >= 14) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе без сопровождения взрослого.");
+        }
+    }
     }
 
     public static void task6() {
